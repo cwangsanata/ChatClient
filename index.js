a2fe7c2303ff526bb358d6f8c738d8af55329114
Chris Wangsanata <chriswangsanata@gmail.com>
2023-10-01 15:55:59 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const { WebSocket } = require('ws');
+const app = express();
+
+const wss = new WebSocket.Server({ 
+    port: 1338,
+    clientTracking: true,
+});
+
+wss.on('connection', (ws) => {
+    ws.send(JSON.stringify({ 
+        sender: 'system',
+        message: 'Welcome to the chat!' 
+    }));
+
+    ws.on('message', (e) => {
+        const rawMessage = Buffer.from(e).toString();
+        try {
+            const { sender, message } = JSON.parse(rawMessage)
+
+            for(const client of wss.clients) {
+                if (client.readyState === WebSocket.OPEN) {
+                    client.send(JSON.stringify({
+                        sender, message
+                    }));
+                }
+            }
+        } catch(error) {
+            console.log('Received non JSON message from client: ', rawMessage);
+        }
+    });
+
+    ws.on('close', () => {
+        console.log('Client disconnected');
+    });
+});
+
+// Serve static files from the public directory
+app.use(express.static('public'));
+
+const port = 1337;
+app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
+});
